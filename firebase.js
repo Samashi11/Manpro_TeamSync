bc61006d8058f468c2975cd5e9a88de0833394c1
Samashi11 <maulashidqic10@gmail.com>
2026-06-04 12:27:40 +0700

    Initialize & Dashboard Page

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAn1kK9yI3xdvS3_aOpCW1KDtQW2e7UBJU",
+  authDomain: "teamsync-5e4c2.firebaseapp.com",
+  projectId: "teamsync-5e4c2",
+  storageBucket: "teamsync-5e4c2.firebasestorage.app",
+  messagingSenderId: "242657589018",
+  appId: "1:242657589018:web:850c959416531a4db35ea3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app, "default");
